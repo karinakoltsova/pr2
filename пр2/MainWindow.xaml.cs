0e286dfd08aed6fbfaa337f8d938e0e878144363
Карина Кольцова < @42-5>
2024-09-25 08:37:27 +0500

    Добавьте файлы проекта.

---
+++ пр2/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int R = Convert.ToInt32(n.Text);
+            double result1 = (4 * Math.PI) * (R*R);
+            res.Text = "Sсф = " + result1.ToString();
+            double result2 = (result1 * R) / 3;
+            V.Text = "Vсф = " + result2.ToString();
+        }
+    }
+}
